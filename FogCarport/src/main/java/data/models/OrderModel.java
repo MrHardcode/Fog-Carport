0aa23650598832438f33b523c359ef9d20d41d78
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-04-15 16:43:14 +0200

    Javadoc on OrderModel.java
    Cheeky Javadoc

--- FogCarport/src/main/java/data/models/OrderModel.java
+++ FogCarport/src/main/java/data/models/OrderModel.java
@@ -9,59 +9,107 @@ package data.models;
  *
  * @author Camilla
  */
-
 // simpelt orderobject - skal udviddes i den avancerede algoritme til at tage 
 // højde for kundens ønsker i forhold til materialvalg, taghældning, dimensioner
 // på skur osv. 
+public class OrderModel
+{
 
-public class OrderModel {
     private int height;
     private int length;
     private int width;
     private boolean shed;
 
-    public OrderModel() {
+    public OrderModel()
+    {
     }
 
-    public OrderModel(int height, int length, int width, boolean shed) {
+    public OrderModel(int height, int length, int width, boolean shed)
+    {
         this.height = height;
         this.length = length;
         this.width = width;
         this.shed = shed;
     }
 
-    public int getHeight() {
+    /**
+     * Get the value of height
+     *
+     * @return the value of height
+     */
+    public int getHeight()
+    {
         return height;
     }
 
-    public void setHeight(int height) {
+    /**
+     * Set the value of height
+     *
+     * @param height new value of height
+     */
+    public void setHeight(int height)
+    {
         this.height = height;
     }
 
-    public int getLength() {
+    /**
+     * Get the value of length
+     *
+     * @return the value of length
+     */
+    public int getLength()
+    {
         return length;
     }
 
-    public void setLength(int length) {
+    /**
+     * Set the value of length
+     *
+     * @param length new value of length
+     */
+    public void setLength(int length)
+    {
         this.length = length;
     }
 
-    public int getWidth() {
+    /**
+     * Get the value of width
+     *
+     * @return the value of width
+     */
+    public int getWidth()
+    {
         return width;
     }
 
-    public void setWidth(int width) {
+    /**
+     * Set the value of width
+     *
+     * @param width new value of width
+     */
+    public void setWidth(int width)
+    {
         this.width = width;
     }
 
-    public boolean isShed() {
+    /**
+     * Get the value of shed
+     *
+     * @return the value of shed
+     */
+    public boolean isShed()
+    {
         return shed;
     }
 
-    public void setShed(boolean shed) {
+    /**
+     * Set the value of shed
+     *
+     * @param shed new value of shed
+     */
+    public void setShed(boolean shed)
+    {
         this.shed = shed;
     }
-    
-    
-    
+
 }
