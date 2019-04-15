e01b8ebd83b686c27accdac7ba5fd6dd3012abbf
Camilla <camilla.valerius@gmail.com>
2019-04-15 14:18:55 +0200

    Partslist object containing bom-list

---
+++ FogCarport/src/main/java/data/models/PartslistModel.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data.models;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Camilla
+ */
+public class PartslistModel {
+    ArrayList<MaterialModel> billOfMaterials;
+
+    public PartslistModel(ArrayList<MaterialModel> billOfMaterials) {
+        this.billOfMaterials = billOfMaterials;
+    }
+
+    public ArrayList<MaterialModel> getBillOfMaterials() {
+        return billOfMaterials;
+    }
+    
+}
