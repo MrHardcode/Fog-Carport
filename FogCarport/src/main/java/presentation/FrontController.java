554f91d09d20df9b226cd36cd8c2aa496b4603b9
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-04-17 21:21:34 +0200

    Fixing Warnings.

--- FogCarport/src/main/java/presentation/FrontController.java
+++ FogCarport/src/main/java/presentation/FrontController.java
@@ -8,12 +8,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ *
+ * @author 
+ */
 @WebServlet(name = "FrontController", urlPatterns =
 {
     "/FrontController"
 })
 public class FrontController extends HttpServlet
 {
+
+    private static final long serialVersionUID = 1L;
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException
