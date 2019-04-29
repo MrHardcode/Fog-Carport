f1a9d8a353e5cf8d5600c71b656600cd97580081
Camilla <camilla.valerius@gmail.com>
2019-04-29 21:29:41 +0200

    preparation of raisedRoof calculations

--- FogCarport/src/main/java/logic/RoofRaisedCalc.java
+++ FogCarport/src/main/java/logic/RoofRaisedCalc.java
@@ -1,10 +1,35 @@
-
 package logic;
+
+import data.models.MaterialModel;
+import data.models.OrderModel;
+import data.models.PartslistModel;
+import java.util.Collection;
 
 /**
  *
  * @author Camilla
  */
 public class RoofRaisedCalc {
+
+    public PartslistModel getRoofRaisedMaterials(OrderModel order) {
+        PartslistModel roofRaisedBOM = new PartslistModel();
+
+        roofRaisedBOM.getBillOfMaterials().addAll(getRoofTiles(order).getBillOfMaterials());
+        roofRaisedBOM.getBillOfMaterials().addAll(getRoofStructure(order).getBillOfMaterials());
+
+        return roofRaisedBOM;
+
+    }
     
+    public PartslistModel getRoofTiles(OrderModel order){
+        PartslistModel roofTilesBOM = new PartslistModel();
+        
+        return roofTilesBOM;
+    }
+    
+    public PartslistModel getRoofStructure(OrderModel order){
+        PartslistModel roofStructureBOM = new PartslistModel();
+        
+        return roofStructureBOM;
+    }
 }
