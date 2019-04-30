d79a097f86dfb84d010920d29bcd62c193322496
Camilla <camilla.valerius@gmail.com>
2019-04-30 22:03:15 +0200

    Commit before merge of task 35

--- FogCarport/src/main/java/logic/RoofRaisedCalc.java
+++ FogCarport/src/main/java/logic/RoofRaisedCalc.java
@@ -1,6 +1,8 @@
 package logic;
 
 
+import data.DataFacade;
+import data.DataFacadeImpl;
 import data.models.OrderModel;
 import data.models.PartslistModel;
 
@@ -10,7 +12,8 @@ import data.models.PartslistModel;
  * @author Camilla
  */
 public class RoofRaisedCalc {
-
+    
+    
     public PartslistModel getRoofRaisedMaterials(OrderModel order) {
         PartslistModel roofRaisedBOM = new PartslistModel();
         
@@ -31,6 +34,17 @@ public class RoofRaisedCalc {
     protected PartslistModel getRoofStructure(OrderModel order){
         PartslistModel roofStructureBOM = new PartslistModel();
         
+        double angleRad = Math.toRadians(order.getIncline()); 
+        double adjacentCath = (order.getWidth() * 0.5) + 300;
+        double hypotenuse = (adjacentCath/Math.cos(angleRad));
+        double oppositeCath = (Math.sin(angleRad) * hypotenuse);
+        
+        if(order.getWidth() < 1800){
+            roofStructureBOM.addMaterial(material);
+        }
+        
         return roofStructureBOM;
     }
+    
+    
 }
