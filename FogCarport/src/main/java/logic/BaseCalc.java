9cd8ae8e405295b19c875da261d21ea3c42f245a
HrBjarup <cph-ab363@cphbusiness.dk>
2019-05-02 00:05:32 +0200

    One small step for the project - one big step for little Asger

--- FogCarport/src/main/java/logic/BaseCalc.java
+++ FogCarport/src/main/java/logic/BaseCalc.java
@@ -1,10 +1,216 @@
 package logic;
 
+import data.DataFacade;
+import data.DataFacadeImpl;
+import data.exceptions.LoginException;
+import data.models.MaterialModel;
+import data.models.OrderModel;
+import data.models.PartslistModel;
+
 /**
  *
  * @author Asger
  */
 public class BaseCalc
 {
+    //Post-rules
+    private int postDistanceStandard = 3100;
+    private int postDistanceRaisedRoof = 2750;
     
+    //IDs of materials from DB
+    private int postID = 4;
+    private int strapID = 54;
+    private int boltID = 24;
+    
+    protected PartslistModel addBase(PartslistModel bom, OrderModel order) throws LoginException
+    {
+        DataFacade db = DataFacadeImpl.getInstance();
+        
+        int carportLength = order.getLength();
+        int carportWidth = order.getWidth();
+        int shedLength = order.getShed_length();
+        int shedWidth = order.getShed_width();
+        boolean hasRaisedRoof = false;
+        if (order.getIncline() > 0)
+        {
+            hasRaisedRoof = true;
+        }
+        
+        calcMaterials(bom, carportLength, carportWidth, shedLength, shedWidth, hasRaisedRoof, db);
+        
+        return bom;
+    }
+    
+    protected void calcMaterials(
+            PartslistModel bom, 
+            int cLength, int cWidth, 
+            int sLength, int sWidth, 
+            boolean heavyRoof, 
+            DataFacade db) throws LoginException
+    {
+        //97x97mm post
+        MaterialModel post = db.getMaterial(postID);
+        int postAmount = 0;
+        if (heavyRoof)
+        {
+            postAmount = calcPostsRaisedRoof(cLength, cWidth, sLength, sWidth);
+        }
+        else
+        {
+            //postAmount = calcPostsFlatRoof(cLength, cWidth, sLength, sWidth);
+        }
+        post.setQuantity(postAmount);
+        //45x195mm rafter wood
+        MaterialModel strap = db.getMaterial(strapID);
+        int strapAmount = calcStraps(cLength, cWidth);
+        strap.setQuantity(strapAmount);
+        
+        //10x120mm bolts
+        MaterialModel bolts = db.getMaterial(boltID);
+        int boltAmount = calcBolts(postAmount, strapAmount);
+        bolts.setQuantity(boltAmount);
+    }
+
+    protected int calcPostsRaisedRoof(int cLength, int cWidth, int sLength, int sWidth)
+    {
+        //Total post amount
+        int postAmount = 0;
+        
+        //If the shed is as wide as the carport:
+        if (sWidth == cWidth)
+        {
+            postAmount = calcPostsFullWidthShed(cLength, cWidth, sLength, sWidth);
+        }
+        //If the shed's width is shorter than the carport's width:
+        else
+        {
+            postAmount = calcPostsOddShed(cLength, cWidth, sLength, sWidth);
+        }
+        return postAmount;
+    }
+    
+    private int calcPostsFullWidthShed(int cLength, int cWidth, int sLength, int sWidth)
+    {
+        //The first posts are always places 800mm from the entrance-end of the carport
+        int length = cLength - 800;
+        //Posts
+        int postAmount = 0;
+
+        /*   Both sides of the carport   */
+        //Counter used to count the amount of times a post should be placed 
+        //between the front post and the first shed post
+        int count = 0;
+        //Adding post amount to the counter plus 1 (to include the front post)
+        count = ((length - sLength) / postDistanceRaisedRoof) + 1;
+        //Another counter
+        int temp = 0;
+        //Adding the count to temp
+        temp += count;
+        //This if-statement makes sure to remove posts where there would have been placed 
+        //two posts inside each other because the shed has corners placed on a point 
+        //where another post has already been placed (not counting the corners of the carport). 
+        //Yeah... it's hard to explain 
+        if (cLength - (800 + postDistanceRaisedRoof * (temp - 1)) == sLength)
+        {
+            postAmount -= 2;
+        }
+        //Adding 2 to temp because: 
+        //1 for the first corner of the shed
+        //Another 1 for the second corner of the shed (which is also the corner of the carport)
+        temp += 2;
+        //Adding temp to the postAmount twice since the sides of the carport are equal to each other 
+        //because the shed has the same width as the carport
+        postAmount += temp * 2;
+        //If the shed is very long we need one or more extra posts
+        if ((sLength / postDistanceRaisedRoof) > 0)
+        {
+            //We add the amount twice since the sides of the carport are equal
+            postAmount += (sLength / postDistanceRaisedRoof) * 2;
+        }
+        /*   The rear of the carport   */
+        //Calculating width. We don't have to think about extra posts for the shed 
+        //since the shed has the same width as the carport
+        //The corner posts have already been calculated
+        if ((cWidth / postDistanceRaisedRoof) > 0)
+        {
+            postAmount += (cWidth / postDistanceRaisedRoof);
+        }
+        return postAmount;
+    }
+    
+    private int calcPostsOddShed(int cLength, int cWidth, int sLength, int sWidth)
+    {
+        //The first posts are always places 800mm from the entrance-end of the carport
+        int length = cLength - 800;
+        //Posts
+        int postAmount = 0;
+        
+        /*   First side of the carport   */
+        //Counter used to count the amount of times a post should be placed 
+        //between the front post and the first shed post
+        int count = 0;
+        //Adding post amount to the counter plus 1 (to include the front post)
+        count = ((length - sLength) / postDistanceRaisedRoof) + 1;
+        //Another counter
+        int temp = 0;
+        //Adding the count to temp
+        temp += count;
+        //Another one of those hard to explain if-statements
+        if (cLength - (800 + postDistanceRaisedRoof * (temp - 1)) == sLength)
+        {
+            postAmount -= 1;
+        }
+        //Adding 2 to temp because: 
+        //1 for the first corner of the shed
+        //Another 1 for the second corner of the shed (which is also the corner of the carport)
+        temp += 2;
+        //Adding temp to the postAmount
+        postAmount += temp;
+        //If the shed is very long we need one or more extra posts
+        if ((sLength / postDistanceRaisedRoof) > 0 && (sLength % postDistanceRaisedRoof != 0))
+        {
+            postAmount += (sLength / postDistanceRaisedRoof);
+        }
+
+        /*   Second side of the carport   */
+        //Adding the amount of posts for the second side. The + 1 is the front post
+        postAmount += (length / postDistanceRaisedRoof) + 1;
+        //Adding an extra post if the previous division didn't go up
+        if (length % postDistanceRaisedRoof > 0)
+        {
+            ++postAmount;
+        }
+
+        /*   The rear of the carport   */
+        //Adding one for the corner of the shed
+        ++postAmount;
+        //Adding extra posts for the part that the shed is covering if the shed is wide enough
+        if (sWidth / postDistanceRaisedRoof > 0 && (sWidth % postDistanceRaisedRoof != 0))
+        {
+            postAmount += (sWidth / postDistanceRaisedRoof);
+        }
+        int restWidth = cWidth - sWidth;
+        //Adding extra posts for the part that the shed doesn't cover if needed
+        if (restWidth / postDistanceRaisedRoof > 0 && (restWidth % postDistanceRaisedRoof != 0))
+        {
+            postAmount += (restWidth / postDistanceRaisedRoof);
+        }
+        return postAmount;
+    }
+
+    protected int calcPostsFlatRoof(int cLength, int cWidth, boolean shedContact, int sLength)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    protected int calcStraps(int cLength, int cWidth)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    protected int calcBolts(int postAmount, int strapAmount)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
---
+++ FogCarport/src/test/java/logic/BaseCalcTest.java
@@ -0,0 +1,139 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logic;
+
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+/**
+ *
+ * @author Asger
+ */
+@RunWith(Parameterized.class)
+public class BaseCalcTest
+{
+    private BaseCalc bc = new BaseCalc();
+
+    private int expected, cLength, cWidth, sLength, sWidth;
+
+    @Parameterized.Parameters
+    public static Collection getTestParameters() {
+        return Arrays.asList(new Object[][]{
+            {10, 7500, 3000, 200, 2000}, {10, 7500, 2000, 200, 1200}, 
+            {11, 7500, 3000, 200, 3000}, {10, 7500, 2000, 200, 2000},
+            {7, 5500, 4000, 1950, 4000}, {8, 5500, 5000, 1950, 1500}});
+    }
+
+    //constructor uses @Parameters
+    public BaseCalcTest(int n1, int n2, int n3, int n4, int n5) {
+        expected = n1;
+        cLength = n2;
+        cWidth = n3;
+        sLength = n4;
+        sWidth = n5;
+    }
+    
+    /**
+     * Test of calcPostsRaisedRoof method, of class BaseCalc.
+     */
+    @Test
+    public void testCalcPostsRaisedRoof()
+    {
+        int result = bc.calcPostsRaisedRoof(cLength, cWidth, sLength, sWidth);
+        assertEquals(expected, result);
+    }
+    
+    //    /**
+//     * Test of addBase method, of class BaseCalc.
+//     */
+//    @Test
+//    public void testAddBase() throws Exception
+//    {
+//        System.out.println("addBase");
+//        PartslistModel bom = null;
+//        OrderModel order = null;
+//        BaseCalc instance = new BaseCalc();
+//        PartslistModel expResult = null;
+//        PartslistModel result = instance.addBase(bom, order);
+//        assertEquals(expResult, result);
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of calcMaterials method, of class BaseCalc.
+//     */
+//    @Test
+//    public void testCalcMaterials() throws Exception
+//    {
+//        System.out.println("calcMaterials");
+//        PartslistModel bom = null;
+//        int cLength = 0;
+//        int cWidth = 0;
+//        int sLength = 0;
+//        int sWidth = 0;
+//        boolean heavyRoof = false;
+//        DataFacade db = null;
+//        BaseCalc instance = new BaseCalc();
+//        instance.calcMaterials(bom, cLength, cWidth, sLength, sWidth, heavyRoof, db);
+//        fail("The test case is a prototype.");
+//    }
+    
+//
+//    /**
+//     * Test of calcPostsFlatRoof method, of class BaseCalc.
+//     */
+//    @Test
+//    public void testCalcPostsFlatRoof()
+//    {
+//        System.out.println("calcPostsFlatRoof");
+//        int cLength = 0;
+//        int cWidth = 0;
+//        boolean shedContact = false;
+//        int sLength = 0;
+//        BaseCalc instance = new BaseCalc();
+//        int expResult = 0;
+//        int result = instance.calcPostsFlatRoof(cLength, cWidth, shedContact, sLength);
+//        assertEquals(expResult, result);
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of calcStraps method, of class BaseCalc.
+//     */
+//    @Test
+//    public void testCalcStraps()
+//    {
+//        System.out.println("calcStraps");
+//        int cLength = 0;
+//        int cWidth = 0;
+//        BaseCalc instance = new BaseCalc();
+//        int expResult = 0;
+//        int result = instance.calcStraps(cLength, cWidth);
+//        assertEquals(expResult, result);
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of calcBolts method, of class BaseCalc.
+//     */
+//    @Test
+//    public void testCalcBolts()
+//    {
+//        System.out.println("calcBolts");
+//        int postAmount = 0;
+//        int strapAmount = 0;
+//        BaseCalc instance = new BaseCalc();
+//        int expResult = 0;
+//        int result = instance.calcBolts(postAmount, strapAmount);
+//        assertEquals(expResult, result);
+//        fail("The test case is a prototype.");
+//    }
+    
+}
