3523ad307ffb5a9381d13f8864ed3c9092d7fda7
Camilla <camilla.valerius@gmail.com>
2019-05-05 23:06:20 +0200

    Unused imports deleted

--- FogCarport/src/test/java/logic/Calculations/RoofRaisedCalcTest.java
+++ FogCarport/src/test/java/logic/Calculations/RoofRaisedCalcTest.java
@@ -5,10 +5,8 @@
  */
 package logic.Calculations;
 
-import logic.Calculations.RoofRaisedCalc;
 import data.DataFacade;
 import data.DataFacadeImpl;
-import data.exceptions.LoginException;
 import data.models.MaterialModel;
 import data.models.OrderModel;
 import data.models.PartslistModel;
