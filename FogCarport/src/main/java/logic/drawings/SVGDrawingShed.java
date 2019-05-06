5e18ad16e15a602ef77146b1d34c97a4a17a6b49
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-05-06 11:22:36 +0200

    Updated field names to be more clear.

--- FogCarport/src/main/java/logic/drawings/SVGDrawingShed.java
+++ FogCarport/src/main/java/logic/drawings/SVGDrawingShed.java
@@ -25,32 +25,32 @@ public class SVGDrawingShed
     
     // Update with a forloop for the amount of poles.
     public String getShedDrawing(OrderModel order){
-        int cw = order.getWidth()/10;
-        int cl = order.getLength()/10;
-        int sw = order.getShed_width()/10;
-        int sl = order.getShed_length()/10;
+        int carportWidth = order.getWidth()/10;
+        int carportLength = order.getLength()/10;
+        int shedWidth = order.getShed_width()/10;
+        int shedLength = order.getShed_length()/10;
         
         
         String SVG = 
                 // Container that matches the entire carport in size.
                 "<svg "
-                + "width=\""+(cw)+"\" "
-                + "height=\""+(cl)+"\">\n" +
+                + "width=\""+(carportWidth)+"\" "
+                + "height=\""+(carportLength)+"\">\n" +
                 
                 // The shed itself.
 "            <rect "
-                + "x=\""+((cw)-(sw))+"\" \n" +
-"                  y=\""+((cl)-(sl))+"\" \n" +
-"                  width=\""+(sw)+"\" \n" +
-"                  height=\""+(sl)+"\" \n" +
+                + "x=\""+((carportWidth)-(shedWidth))+"\" \n" +
+"                  y=\""+((carportLength)-(shedLength))+"\" \n" +
+"                  width=\""+(shedWidth)+"\" \n" +
+"                  height=\""+(shedLength)+"\" \n" +
 "                  style=\"stroke:black;stroke-dasharray:10,5;stroke-width:5;fill-opacity:0.1;stroke-opacity:1\" />\n" +
                 
                 // Posts from corner to the edge.
 "            \n" + getPole(order, 0) + getPole(order, 90) +
                 
                 //Post at the corner
-                "<rect x=\"" + (((cw) - (sw))) + "\" \n"
-                    + "y=\"" + ((cl) - (sl)) + "\" \n"
+                "<rect x=\"" + (((carportWidth) - (shedWidth))) + "\" \n"
+                    + "y=\"" + ((carportLength) - (shedLength)) + "\" \n"
                     + "width=\"15\" height=\"15\" \n"
                     + "style=\"stroke:black;stroke-width:5;fill-opacity:0;stroke-opacity:1\" />\n"+
 "            Sorry, your browser does not support inline SVG.  \n" +
@@ -62,31 +62,31 @@ public class SVGDrawingShed
     // 90 in angle if its the width. 0 if the length.
     String getPole(OrderModel order, int angle)
     {
-        int cw = order.getWidth() / 10;
-        int cl = order.getLength() / 10;
-        int sw = order.getShed_width() / 10;
-        int sl = order.getShed_length() / 10;
+        int carportWidth = order.getWidth() / 10;
+        int carportLength = order.getLength() / 10;
+        int shedWidth = order.getShed_width() / 10;
+        int shedLength = order.getShed_length() / 10;
         String SVG = "";
 
         if (angle == 0) // Horizontally
         { // Using example where shed is 640cm wide and postdistance is 310cm.
-            int postamount = sw / postdistance; // 640/310 = 2 as int 
+            int postamount = shedWidth / postdistance; // 640/310 = 2 as int 
             for (double i = 0; i <= postamount; i += 1)
             {                           
                 double postplacement = ((1 + i) / (1 + postamount)); // (1+0)/(1+2) = 0.333333
-                double tempint = (double) (sw * postplacement); // 0.333333 * 640 = 213
+                double tempint = (double) (shedWidth * postplacement); // 0.333333 * 640 = 213
                 int postwidth = (int) tempint;
                 
                 // The Arrow.
                 if (i == 0)
                 {                   // beginX     beginY         endX                     endY 
-                    SVG += getArrow(((cw - sw)), (cl - sl)-15, ((cw - sw) + postwidth), (cl - sl)-15);
-                    SVG += getLabel((cw - sw)+(postwidth/2)-10, (cl - sl)-20, postwidth);
+                    SVG += getArrow(((carportWidth - shedWidth)), (carportLength - shedLength)-15, ((carportWidth - shedWidth) + postwidth), (carportLength - shedLength)-15);
+                    SVG += getLabel((carportWidth - shedWidth)+(postwidth/2)-10, (carportLength - shedLength)-20, postwidth);
                 }
                 
                 // The post
-                SVG += " <rect x=\"" + ((cw - sw) + postwidth) + "\" \n" // Places posts 213cm instead of 310cm. Now they are spread even and nice.
-                        + "y=\"" + (cl - sl) + "\" \n"
+                SVG += " <rect x=\"" + ((carportWidth - shedWidth) + postwidth) + "\" \n" // Places posts 213cm instead of 310cm. Now they are spread even and nice.
+                        + "y=\"" + (carportLength - shedLength) + "\" \n"
                         + "width=\"15\" height=\"15\" \n"
                         + "style=\"stroke:black;stroke-width:5;fill-opacity:0;stroke-opacity:1\" />\n ";
             }
@@ -94,22 +94,22 @@ public class SVGDrawingShed
 
         if (angle == 90) // Vertically 
         {
-            int postamount = sl / postdistance; 
+            int postamount = shedLength / postdistance; 
             for (double i = 0; i <= postamount; i += 1)
             {                           
                 double postplacement = ((1 + i) / (1 + postamount));
-                double tempint = (double) (sl * postplacement); 
+                double tempint = (double) (shedLength * postplacement); 
                 int postlength = (int) tempint;
                 
                 // The Arrow.
                 if (i == 0)
                 {                   // beginX       beginY                  endX            endY                                
-                    SVG += getArrow((cw - sw)-15, ((cl - sl)), (cw - sw)-15, ((cl - sl) + postlength));
-                    SVG += getLabel((cw - sw)-60, (cl - sl)+(postlength/2), postlength);
+                    SVG += getArrow((carportWidth - shedWidth)-15, ((carportLength - shedLength)), (carportWidth - shedWidth)-15, ((carportLength - shedLength) + postlength));
+                    SVG += getLabel((carportWidth - shedWidth)-60, (carportLength - shedLength)+(postlength/2), postlength);
                 }
 
-                SVG += " <rect x=\"" + (cw - sw)  + "\" \n"
-                        + "y=\"" + ((cl - sl) + postlength) + "\" \n"
+                SVG += " <rect x=\"" + (carportWidth - shedWidth)  + "\" \n"
+                        + "y=\"" + ((carportLength - shedLength) + postlength) + "\" \n"
                         + "width=\"15\" height=\"15\" \n"
                         + "style=\"stroke:black;stroke-width:5;fill-opacity:0;stroke-opacity:1\" />\n ";
             }
