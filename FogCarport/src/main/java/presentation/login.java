528db20add13351e8b70ee921ce2b7c4d1d71403
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-05-13 09:53:14 +0200

    login.java exception handling

--- FogCarport/src/main/java/presentation/login.java
+++ FogCarport/src/main/java/presentation/login.java
@@ -9,10 +9,11 @@ import javax.servlet.http.HttpServletRequest;
 import logic.LogicFacade;
 
 /**
- * Login Method.
- * This is called after you hit "log ind" on the login.jsp.
- * You're then either confirmed as a valid user, or rejected and sent back to the login page.
- * @author 
+ * Login Method. This is called after you hit "log ind" on the login.jsp. You're
+ * then either confirmed as a valid user, or rejected and sent back to the login
+ * page.
+ *
+ * @author
  */
 public class login extends Command
 {
@@ -26,15 +27,25 @@ public class login extends Command
     {
         String email = request.getParameter("email"); // Get the email from the Parameters 
         String password = request.getParameter("password"); // Get the password from the Parameters
-        
-        // Check that Customer exists in the Database. If Customer is in Database, return it as a Model.
-        CustomerModel customer = logic.login(email, password);
-        
-        // Place Customer on the Session.
-        request.getSession().setAttribute("customer", customer);
-        
-        // Return Customer to the homepage of the website.
-        return "homepage";
+
+        if (email == null || email.isEmpty())
+        {
+            throw new LoginException("Email field can't be empty.");
+        } else if (password == null || password.isEmpty())
+        {
+            throw new LoginException("Password field can't be empty.");
+        } else
+        {
+            // Check that Customer exists in the Database. If Customer is in Database, return it as a Model.
+            CustomerModel customer = logic.login(email, password);
+
+            // Place Customer on the Session.
+            request.getSession().setAttribute("customer", customer);
+
+            // Return Customer to the homepage of the website.
+            return "homepage";
+        }
+
     }
-    
+
 }
