40abc86c15986827bef6ba7c655de039865250cd
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-05-16 15:34:29 +0200

    Merge conflicts resolved.

--- FogCarport/src/main/java/presentation/viewOrder.java
+++ FogCarport/src/main/java/presentation/viewOrder.java
@@ -6,9 +6,7 @@ package presentation;
 import data.exceptions.AlgorithmException;
 import data.exceptions.LoginException;
 import data.models.OrderModel;
-import data.models.PartslistModel;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import logic.LogicFacade;
 
 /**
@@ -32,9 +30,9 @@ public class viewOrder extends Command {
 
         // Place values used by viewOrder on request.
         request.setAttribute("order", order);
-        request.setAttribute("tile", logic.getMaterial(order.getRoof_tiles_id()).getDescription());
-        request.setAttribute("shedwalls", logic.getMaterial(order.getShed_walls_id()).getDescription());
-        request.setAttribute("shedfloor", logic.getMaterial(order.getShed_floor_id()).getDescription());
+        request.setAttribute("tile", logic.getMaterial(order.getRoof_tiles_id(), "roof").getDescription());
+        request.setAttribute("shedwalls", logic.getMaterial(order.getShed_walls_id(), "shed").getDescription());
+        request.setAttribute("shedfloor", logic.getMaterial(order.getShed_floor_id(), "shed").getDescription());
         request.setAttribute("customer", logic.getCustomer(order.getId_customer()));
         request.setAttribute("employee", logic.getEmployee(order.getId_employee()));
 
