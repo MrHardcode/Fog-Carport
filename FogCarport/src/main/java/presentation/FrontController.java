46f3c699a795961fc480b2bad1d3cfe579c739d0
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-05-20 12:34:13 +0200

    Added better validation.

--- FogCarport/src/main/java/presentation/FrontController.java
+++ FogCarport/src/main/java/presentation/FrontController.java
@@ -65,7 +65,11 @@ public class FrontController extends HttpServlet
         // IF USER IS ON VIEW ORDERS OR VIEW PARTSLIST OR VIEW DRAWINGS AND NOT LOGGED IN
         String command = request.getParameter("command");
         String link = request.getParameter("link");
-        if (customer == null && employee == null && ("viewSVG".equals(link) || "viewSVG".equals(command) || "viewOrder".equals(link) || "viewOrder".equals(command) || "allOrders".equals(command) || "allOrders".equals(link)))
+        if (customer == null && employee == null 
+                && ("viewSVG".equals(link) || "viewSVG".equals(command) 
+                || "viewOrder".equals(link) || "viewOrder".equals(command) 
+                || "allOrders".equals(command) || "allOrders".equals(link) 
+                || "makeCarportForm".equals(link) || "makeCarportForm".equals(command)))
         {
             // INVALIDATE THE FAULTY SESSION.
             session.invalidate();
