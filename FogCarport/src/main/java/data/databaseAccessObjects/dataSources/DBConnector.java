f7c18c69cb6740f55014ee6023ff1011479cd141
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-05-20 09:03:07 +0200

    Moved DataSource to their own folder part two.

--- FogCarport/src/main/java/data/databaseAccessObjects/dataSources/DBConnector.java
+++ FogCarport/src/main/java/data/databaseAccessObjects/dataSources/DBConnector.java
@@ -1,5 +1,5 @@
 
-package data.databaseAccessObjects;
+package data.databaseAccessObjects.dataSources;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
--- FogCarport/src/main/java/data/databaseAccessObjects/dataSources/DataSourceMysql.java
+++ FogCarport/src/main/java/data/databaseAccessObjects/dataSources/DataSourceMysql.java
@@ -1,3 +1,5 @@
+package data.databaseAccessObjects.dataSources;
+
 ///*
 // *  
 // */
