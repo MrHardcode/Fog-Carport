684fdfe63e883bcb8ca0a226843e4abbbe14938a
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-05-27 16:11:34 +0200

    JavaDoc on logic package.

---
+++ FogCarport/src/main/java/logic/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Logic Package.
+ * This package contains the logic for generating SVG drawings and the partslist for a carport.
+ * 
+ * Contains algorithms that uses info from data layer. This package is used by presentation layer, through the LogicFacade.
+ * Patterns used:
+ *  - Facade Pattern: https://en.wikipedia.org/wiki/Facade_pattern
+ *  
+ * The logic layer as part of a threelayer architecture: https://en.wikipedia.org/wiki/Multitier_architecture
+ * 
+ * Also contains an old PartslistLogic class used back in sprint 1.
+ */
+package logic;
