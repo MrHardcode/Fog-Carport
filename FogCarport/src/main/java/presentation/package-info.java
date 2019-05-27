735f7d23e10adea4203411d848061049c08fca1f
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-05-27 16:03:42 +0200

    JavaDoc on presentation package.

---
+++ FogCarport/src/main/java/presentation/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * Presentation Package.
+ * This package contains the classes of the system's Commands and FrontController.
+ * Translates info from Logic layer into something the user can understand.
+ * Patterns used:
+ *  - Command pattern: https://en.wikipedia.org/wiki/Command_pattern
+ *  - Front controller pattern: https://en.wikipedia.org/wiki/Front_controller
+ *  
+ * The presentation layer as part of a threelayer architecture: https://en.wikipedia.org/wiki/Multitier_architecture
+ * 
+ * Also contains a Validation class used to make sure that user inputs pass certain criteria before being processed further.
+ */
+package presentation;
+
+
